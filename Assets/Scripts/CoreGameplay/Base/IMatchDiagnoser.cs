a810b50c9eeaa333c5972c27be4ef17decb6d496
BershadskyArtem <bershadskyartem@gmail.com>
2022-06-30 14:12:59 +0300

    Match Dignoser added
    Match Dignoser added

---
+++ Assets/Scripts/CoreGameplay/Base/IMatchDiagnoser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CoreGameplay.Matches;
+
+namespace CoreGameplay.Base
+{
+    public interface IMatchDiagnoser
+    {
+        public IEnumerable<Match> GetMatchesFromBoard(NodeObject[,] board);
+        public Match GetMatchAtPoint(NodeObject[,] board , int xPos , int yPos);
+        public IMatchDiagnoser AddMatchRule(IMatchRule rule);
+        public IMatchDiagnoser ResetRules();
+    }
+}
---
+++ Assets/Scripts/CoreGameplay/Implementations/MatchDiagnoser.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using CoreGameplay.Base;
+using CoreGameplay.Matches;
+using UnityEngine;
+
+namespace CoreGameplay.Implementations
+{
+    public class MatchDiagnoser : IMatchDiagnoser
+    {
+        private readonly List<IMatchRule> _matchRules;
+
+        public MatchDiagnoser()
+        {
+            _matchRules = new List<IMatchRule>();
+        }
+        
+        public IEnumerable<Match> GetMatchesFromBoard(NodeObject[,] board)
+        {
+            List<Match> result = new List<Match>();
+
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    foreach (var matchRule in _matchRules)
+                    {
+                        var m = new Match(new Vector2Int(x , y) , board[x,y].GetColor());
+                        if (matchRule.TryGetMatchAtPoint(board, x, y, ref m))
+                        {
+                            result.Add(m);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Match GetMatchAtPoint(NodeObject[,] board, int xPos, int yPos)
+        {
+            foreach (var matchRule in _matchRules)
+            {
+                var m = new Match(new Vector2Int(xPos , yPos) , board[xPos , yPos].GetColor());
+                if (matchRule.TryGetMatchAtPoint(board, xPos , yPos, ref m))
+                {
+                    return m;
+                }
+            }
+            return Match.Zero;
+        }
+
+        public IMatchDiagnoser AddMatchRule(IMatchRule rule)
+        {
+            if(_matchRules.Contains(rule))return this;
+            _matchRules.Add(rule);
+            return this;
+        }
+
+        public IMatchDiagnoser ResetRules()
+        {
+            _matchRules.Clear();
+            return this;
+        }
+    }
+}
